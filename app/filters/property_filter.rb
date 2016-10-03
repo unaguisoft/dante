11a929c43578f0f8ea2f4c3b113335d0940911cd
GONZALO <gonzagaldamez@gmail.com>
2016-10-03 17:14:15 -0300

    FilterPropertiesTest y minor bugs

--- app/filters/property_filter.rb
+++ app/filters/property_filter.rb
@@ -1,18 +1,19 @@
 class PropertyFilter
-
+  
   include ActiveModel::Model
 
-  attr_accessor :id, :kind, :price_from, :price_to, :property_kind, :city_id,
-                :user_id, :title
+  attr_accessor :id, :title, :kind, :city_id, :price_from, :price_to, 
+                :property_kind, :kind, :user_id
 
   def call
     properties = Property.all
     properties = properties.where(id: @id) if @id.present?
+    properties = properties.where('title ILIKE ?', "%#{@title}%") if @title.present?
     properties = properties.where(property_kind: @property_kind) if @property_kind.present?
     properties = properties.where(kind: @kind) if @kind.present?
     properties = properties.where(city_id: @city_id) if @city_id.present?
     properties = properties.where(user_id: @user_id) if @user_id.present?
-    properties = properties.where(price: _price_from.._price_to) if @price_from.present? || @price_to.present?
+    properties = properties.where(price_in_cents: _price_from.._price_to) if @price_from.present? || @price_to.present?
 
     properties
   end
@@ -20,10 +21,10 @@ class PropertyFilter
   private
   
   def _price_from
-    @price_from || 0
+    (@price_from.to_d || 0).to_d * 10_000
   end
   
   def _price_to
-    @price_to || 10_000_000
+    (@price_to || 50_000_000).to_d * 10_000
   end
 end
