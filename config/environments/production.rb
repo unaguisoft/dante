62f115c307f15a5440bf6ea5f206793af8d0e4bf
GONZALO <gonzagaldamez@gmail.com>
2016-10-24 19:06:58 -0300

    Add host for environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  
+  Rails.application.routes.default_url_options[:host] = 'danteinmobiliaria.herokuapp.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
