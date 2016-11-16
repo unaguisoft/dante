39937f4f59e607ee14400da619e733692c3c32bc
Nicolás Galdámez <nicogaldamez@gmail.com>
2016-11-16 09:22:52 -0300

    Investments in home page

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -5,6 +5,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @barbie = properties(:casa_de_barbie)
     @mickey = properties(:casa_de_mickey)
+    @apple_store = investments(:apple_store)
+    @samsung_store = investments(:samsung_store)
   end
 
   test 'should display home page' do
@@ -59,6 +61,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get properties_for_web_path, params: { property_filter: { price_to: @mickey.price } }
     assert_includes assigns(:properties), @mickey.decorate
     assert_not_includes assigns(:properties), @barbie.decorate
+  end
+
+  test 'should display investments' do
+    get root_path
+    assert_includes assigns(:presenter).investments, @apple_store
+    assert_not_includes assigns(:presenter).investments, @samsung_store
   end
 
 end
