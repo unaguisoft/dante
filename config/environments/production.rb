ed6582647dd34e6c8b7b12793afcd64e485eaaa6
Nicolás Galdámez <nicogaldamez@MacBook-Pro-de-Nicolas.local>
2017-05-15 20:26:02 -0300

    Changes to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  Rails.application.routes.default_url_options[:host] = 'danteinmobiliaria.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'danteestudioinmobiliario.com.ar'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -75,6 +75,11 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
