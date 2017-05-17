fc908032e717e4868675bbd84deb84fc95175c7c
GONZALO <gonzagaldamez@gmail.com>
2017-05-17 16:40:44 -0300

    Un administrador puede cambiar el agente encargado de una propiedad
    - Se agrega método name_and_email en user decorator
    - Se agrega input para elegir usuario a cargo de la propiedad

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,14 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def name_and_email
+    "#{user.name.humanize}  (#{user.email})"
+  end
+
+  private
+
+  def user
+    object
+  end
 
 end
